636525324ec45c669ee723e17486fc9e347acb76
Miguel Etienne <Etienne.miguel@gmail.com>
2022-11-06 22:39:17 -0500

    first commit

---
+++ app/routes.js
@@ -0,0 +1,191 @@
+const { ObjectId } = require("mongodb");
+
+// need to do put and delete
+module.exports = function (app, passport, db, objectId) {
+  //Bieeg Func from server
+  //split from server (2nd part)
+  // normal routes ===============================================================
+
+  // show the home page (will also have our login links)
+  app.get("/", function (req, res) {
+    res.render("index.ejs");
+  });
+
+  // PROFILE SECTION =========================
+  app.get("/profile", isLoggedIn, function (req, res) {
+    db.collection("messages")
+      .find()
+      .toArray((err, result) => {
+        if (err) return console.log(err);
+        res.render("profile.ejs", {
+          user: req.user,
+          messages: result,
+        });
+      });
+  });
+
+  app.get("/home", isLoggedIn, function (req, res) {
+    db.collection("tiers")
+      .find({ rank: "S-Tier" })
+      .toArray((err, sRank) => {
+        db.collection("tiers")
+          .find({ rank: "A-Tier" })
+          .toArray((err2, aRank) => {
+            db.collection("tiers")
+              .find({ rank: "B-Tier" })
+              .toArray((err3, bRank) => {
+                db.collection("tiers")
+                  .find({ rank: "C-Tier" })
+                  .toArray((err4, cRank) => {
+                    db.collection("tiers")
+                      .find({ rank: "D-Tier" })
+                      .toArray((err5, dRank) => {
+                        db.collection("tiers")
+                          .find({ rank: "E-Tier" })
+                          .toArray((err6, eRank) => {
+                            if (err) return console.log(err);
+                            res.render("home.ejs", {
+                              user: req.user,
+                              sRank: sRank,
+                              aRank: aRank,
+                              bRank: bRank,
+                              cRank: cRank,
+                              dRank: dRank,
+                              eRank: eRank,
+                            });
+                          });
+                      });
+                  });
+              });
+          });
+      });
+  });
+
+  // LOGOUT ==============================
+  app.get("/logout", function (req, res) {
+    req.logout();
+    res.redirect("/");
+  });
+  //loading
+
+  // message board routes ===============================================================
+  //create method
+  app.post("/new", (req, res) => {
+    db.collection("tiers").save(
+      {
+        //req.body is the request from the body. The types are name and msg
+        name: req.body.name,
+        rank: req.body.rank,
+        userId: ObjectId(req.user._id),
+        date: new Date(),
+      },
+      (err, result) => {
+        if (err) return console.log(err);
+        console.log("saved to database");
+        res.redirect("/home");
+      }
+    );
+  });
+  // thumbs up and thumbs down logic
+  // puts and deletes are main js only / fetch
+  app.put("/update", (req, res) => {
+    db.collection("tiers").findOneAndUpdate(
+      {
+        //looking for
+        _id: objectId(req.body._id),
+      },
+      {
+        //the do
+        //delete everything and put this here
+        $set: {
+          _id: ObjectId(req.body._id),
+          name: req.body.name,
+          rank: req.body.rank,
+          userId: req.user._id,
+          date: new Date()
+        },
+      },
+      {
+        sort: { _id: -1 },
+        upsert: false,
+      },
+      (err, result) => {
+        if (err) return res.send(err);
+        res.send(result);
+      }
+    );
+  });
+
+
+  app.delete("/delete", (req, res) => {
+    console.log(req.body.name, req.body._id)
+    db.collection("tiers").findOneAndDelete(
+      { name: req.body.name, _id: ObjectId(req.body._id) },
+      (err, result) => {
+        if (err) return res.send(500, err);
+        res.send(result);
+      }
+    );
+  });
+
+  // =============================================================================
+  // AUTHENTICATE (FIRST LOGIN) ==================================================
+  // =============================================================================
+
+  // locally --------------------------------
+  // LOGIN ===============================
+  // show the login form
+  app.get("/login", function (req, res) {
+    res.render("login.ejs", { message: req.flash("loginMessage") });
+  });
+
+  // process the login form
+  app.post(
+    "/login",
+    passport.authenticate("local-login", {
+      successRedirect: "/home", // redirect to the secure profile section
+      failureRedirect: "/login", // redirect back to the signup page if there is an error
+      failureFlash: true, // allow flash messages
+    })
+  );
+
+  // SIGNUP =================================
+  // show the signup form
+  app.get("/signup", function (req, res) {
+    res.render("signup.ejs", { message: req.flash("signupMessage") });
+  });
+
+  // process the signup form
+  app.post(
+    "/signup",
+    passport.authenticate("local-signup", {
+      successRedirect: "/home", // redirect to the secure profile section
+      failureRedirect: "/signup", // redirect back to the signup page if there is an error
+      failureFlash: true, // allow flash messages
+    })
+  );
+
+  // =============================================================================
+  // UNLINK ACCOUNTS =============================================================
+  // =============================================================================
+  // used to unlink accounts. for social accounts, just remove the token
+  // for local account, remove email and password
+  // user account will stay active in case they want to reconnect in the future
+
+  // local -----------------------------------
+  app.get("/unlink/local", isLoggedIn, function (req, res) {
+    var user = req.user;
+    user.local.email = undefined;
+    user.local.password = undefined;
+    user.save(function (err) {
+      res.redirect("/profile");
+    });
+  });
+};
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+
+  res.redirect("/");
+}
---
+++ public/main.js
@@ -0,0 +1,62 @@
+
+document.querySelectorAll('.deleteBtnI').forEach(element => {
+  element.addEventListener('click', (e)=>{
+    let id = e.target.parentElement.children[1].innerText
+    let name = e.target.parentElement.parentElement.children[0].innerText.toLowerCase()
+    console.log(e.target.parentElement.parentElement.children[0].innerText, e.target.parentElement.children[1].innerText)
+    fetch('delete', {
+      method: 'delete',
+      headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+      body: JSON.stringify({
+        'name': name,
+        '_id': id
+      })
+    })
+    .then(response => {
+      if (response.ok) return response.json()
+    })
+    .then(data => {
+      window.location.reload(true)
+    })
+  })
+})
+
+document.querySelectorAll('.editBtnI').forEach(element => {
+    element.addEventListener('click', (e)=>{
+      console.log(e.target.parentElement.parentElement.children)
+      let name = prompt('1.) Enter new Item name', e.target.parentElement.parentElement.children[0].innerText)
+        if(name){
+          var rank = prompt('2.) Now enter new Item rank, ex. B-Tier')
+          let rankArray = [
+            'S-Tier',
+            'A-Tier',
+            'B-Tier',
+            'C-Tier',
+            'D-Tier',
+            'E-Tier'
+          ]
+          if(!rankArray.includes(rank)){
+            alert('Please enter a valid rank, ex. C-Tier')
+            return
+          }
+        }else{
+          alert('You didnt enter anything!')
+          return
+        }
+      let id = e.target.parentElement.parentElement.children[2].children[1].innerText
+      fetch('update', {
+        method: 'put',
+        headers: {'Content-Type': 'application/json', 'Accept': 'application/json'},
+        body: JSON.stringify({
+          'name':name,
+          'rank':rank,
+          '_id': id
+        })
+      }).then(response => {
+        if (response.ok) return response.json()
+      })
+      .then(data => {
+        window.location.reload(true)
+      })
+    })
+  })
